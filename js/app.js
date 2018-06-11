5e091f43ccf22370c7e095310d2e315a43273cf2
Dennis Tobi <denkoksz@gmail.com>
2018-06-11 11:33:08 +0200

    elso ket feladat JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -13,6 +15,33 @@ function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
   console.log(userDatas);
+  // 1. A kapott adatokat rendezd ár(cost_in_creadits) szerint növekvő sorrendbe.
+  novekvoSorrend(userDatas);
+  consumaleDelete(userDatas);
+}
+function novekvoSorrend(array){
+  for (var i = array.length; i > 0; i--) {
+    var tmp;
+    for (var j = 0; j < i - 1; j++) {
+      if (array[j].cost_in_credits > array[j + 1].cost_in_credits) {
+        tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
+  console.log(array);
+  return array;
+}
+// 2. Töröld az összes olyan adatot, ahol a consumables értéke NULL. Fontos, hogy ne csak undefined-ra állítsd a tömbelemet!!!
+function consumaleDelete(array){
+  for(var i = 0; i < array.length; i++){
+    if(array[i].consumables == null){
+      array.splice(i, 1);
+    }
+  }
+  console.log(array);
+  return array;
 }
 
 getData('/json/spaceships.json', successAjax);
